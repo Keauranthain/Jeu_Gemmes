bca1283c82520a76af641dc1fabd17a3a3c5a8ed
Keauranthain <corentin.stark@free.fr>
2025-05-27 20:41:20 +0200

    Test combat plus logique

--- Test_Combat.py
+++ Test_Combat.py
@@ -4,14 +4,14 @@ from Combat import Combat
 import Personnage as perso
 
 if __name__ == '__main__':
-    humain_1 = perso.Personnage(joueur=False, nom="Titi")
+    humain_1 = perso.Personnage(joueur=True, nom="Titi")
     humain_2 = perso.Personnage(joueur=False, nom="Toto")
     goblin_1 = perso.Personnage(joueur=False, nom="Goblin 1", race="goblin")
     goblin_2 = perso.Personnage(joueur=False, nom="Goblin 2", race="goblin")
     goblin_3 = perso.Personnage(joueur=False, nom="Goblin 3", race="goblin")
     humains = [humain_1, humain_2]
     goblins = [goblin_1, goblin_2, goblin_3]
-    c = Combat(humains, goblins,affiche=False)
+    c = Combat(humains, goblins)
     start = time.time()
     c.debut()
     end = time.time()
